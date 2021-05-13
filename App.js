2fba635d9cdef06aed40e185c6eccf0278c4263d
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-05-13 09:21:20 +0530

    a

--- App.js
+++ App.js
@@ -1,24 +1,52 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import { Text, View } from 'react-native';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 
-const HelloWorldApp = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>how are you !</Text>
+const image = {
+  uri: 'https://i.pinimg.com/originals/99/a2/dc/99a2dcfa8eade86cdcc9ac747d75fae5.jpg',
+};
+
+const App = () => (
+  <View style={styles.container}>
+    <View style = {styles.backgroundContainer}>
+    <ImageBackground source={image} style={styles.backdrop} />
     </View>
-  )
-}
-export default HelloWorldApp;
+    <View style = {styles.overlay}>
+      <Text style = {styles.headline}>WAIIIIFU</Text>
+    </View>
+  </View>
+);
+
+
+
+
+const styles = StyleSheet.create({
+  backgroundContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  overlay: {
+    opacity: 0.8,
+    backgroundColor: 'transparent'
+  },
+  backdrop: {
+    flex:1,
+    flexDirection: 'column'
+  },
+  headline: {
+    top: 8,
+    color: "white",
+    fontSize: 26,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "transparent"
+  }
+});
+
+export default App;
